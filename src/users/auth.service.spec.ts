9677ef06f679d426a625072a436f4baed6ef1d4e
Ahmed564676786 <ahmedking4660@gmail.com>
2026-09-08 09:06:39 +0500

    8/9/26

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,32 +1,75 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
-import { UsersService } from './users.service';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { AuthService } from './auth.service';
+// import { UsersService } from './users.service';
 
-describe('AuthService', () => {
-  let authService: AuthService;
+import { User } from "./entities/user.entity";
+import { UsersService } from "./users.service";
 
 
-  const fakeUserService = {
+  describe('AuthService',()=>{
 
-      find:() => Promise.resolve([]),
-      create: (email: string, name: string, password: string) => Promise.resolve({id: 1, email, name, password}),
-  }
+    let service : AuthService;
+    let fakeUsersService : Partial<UsersService>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        {
-          provide: UsersService,
-          useValue: fakeUserService,
-        },
-      ],
-    }).compile();
 
-    authService = module.get<AuthService>(AuthService);
+    beforeEach(async () =>{
+
+
+      // Mock Implementation of UsersService
+   
+      fakeUsersService = {
+        isEmailTaken: (email) => Promise.resolve([]),
+        create : (email:string,password:string) => Promise.resolve({id:1,email,password})
+      }
+
+      const module  = await Test.createTestingModule({
+
+         providers:[AuthService,{
+            provide:UsersService,
+            useValue:fakeUsersService
+         }]
+      }).compile();
+
+      service = module.get(AuthService);
+  })
+
+    
+  it('can create an instance of AuthService', async () => {
+      expect(service).toBeDefined();
+  })
+
+
+  // 3rd test
+  it('throws an error if the email is already taken',async ()=>{
+
+
+    // npx jest auth.service.spec.ts -t "create a new user with a salted and hashed password"
+
+    fakeUsersService.isEmailTaken = () => Promise.resolve([{ id: 1 } as User]);
+
+    await expect(service.signup('ali','ali23@gmail.com','12345')).rejects.toThrow();
+
   });
 
-  it('can create an instance of AuthService', () => {
-    expect(authService).toBeDefined();
+
+  // 2nd test
+  it('create a new user with a salted and hashed password',async ()=>{
+
+
+    // npx jest auth.service.spec.ts -t "create a new user with a salted and hashed password"
+
+    const user = await service.signup('ali','ali23@gmail.com','12345')
+    expect(user.password).not.toEqual('12345');
+
+    const [salt,hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+
+    expect(hash).toBeDefined();
+
   });
-});
+
+
+
+
+  })
